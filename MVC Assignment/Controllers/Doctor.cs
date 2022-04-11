cbc529da0fcc4e60c72fe9dec8c534f581150913
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-04-11 17:47:03 +0200

    Temperature form created. Checking for hypothermia

--- MVC Assignment/Controllers/Doctor.cs
+++ MVC Assignment/Controllers/Doctor.cs
@@ -8,9 +8,31 @@ namespace MVC_Assignment.Controllers
 {
     public class Doctor : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(float temp)
+        {
+            ViewBag.fever = "You look healty. Time to work!";
+            if (temp >= 37)
+            {
+                ViewBag.fever = "I, as a computer, would say you got some fever";
+            }
+            if (temp < 35)
+            {
+                ViewBag.fever = "I, as a computer, would say you are more or less dead";
+            }
+        return View();
+        }
+               
     }
 }
+
+
+
+
+
