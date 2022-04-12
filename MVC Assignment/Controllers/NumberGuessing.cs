1d637266f97aa20258efe9dcb1e67e7bda0c5ebf
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-04-12 11:07:36 +0200

    Guessnumber working but have to add sessions and routing part.

---
+++ MVC Assignment/Controllers/NumberGuessing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Assignment.Models;
+
+namespace MVC_Assignment.Controllers
+{
+    public class NumberGuessing : Controller
+    {       
+        [HttpGet]
+        public IActionResult Index()
+        {
+            NumberGuessingService.GetNumber();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int guess)
+        {
+            ViewBag.response = NumberGuessingService.EvaluateGuess(guess);
+            return View();
+        }
+
+    }
+}
---
+++ MVC Assignment/Models/NumberGuessingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Assignment.Models
+{
+    public class NumberGuessingService
+    {
+        static int guesses;
+        static int number;
+        static public int GetNumber() {
+            Random rnd = new Random();
+            number=rnd.Next(1, 101);
+            return number;
+        }
+        static public string EvaluateGuess(int guess)
+        {
+            string response;
+            guesses++;
+            if (guess == number)
+            {
+                response = $"Congratulation you guessed right in {guesses} guesses!";
+                //RedirectToAction("Index");
+            }
+            else
+            {
+                response = $"You have guessed {guesses} so fare!"; 
+                response += $" The number i am thingking of is {((guess < number) ? "higher" : "lesser")} then you guessed";
+            }
+            return response;
+        }
+    }
+}
