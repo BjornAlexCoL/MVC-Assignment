b0cc608c55a6f9fd08035ad8a1ba7f1b7bba12fb
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-04-14 11:14:03 +0200

    Highscores Cookies added. Need to be sorted.

--- MVC Assignment/Controllers/NumberGuessing.cs
+++ MVC Assignment/Controllers/NumberGuessing.cs
@@ -18,18 +18,16 @@ namespace MVC_Assignment.Controllers
             HttpContext.Session.SetInt32("Number", rnd.Next(1, 101));
             HttpContext.Session.SetInt32("Guesses", 0);
 
-            string HighScores = HttpContext.Session.GetString("HighScores");
-            if (HighScores != null)
-            {
-                ViewBag.Memos = HighScores;
-            }
+            ViewBag.highScores = Request.Cookies["HighScores"];
 
+            ViewBag.AddToHighScore = false;
             return View();
         }
+
         [HttpPost]
         public IActionResult Index(int guess)
         {
-            int guesses= (int)HttpContext.Session.GetInt32("Guesses");
+            int guesses = (int)HttpContext.Session.GetInt32("Guesses");
             int number = (int)HttpContext.Session.GetInt32("Number");
 
             HttpContext.Session.SetInt32("Guesses", ++guesses);
@@ -37,16 +35,40 @@ namespace MVC_Assignment.Controllers
             if (guess == number)
             {
                 ViewBag.response = $"Congratulation you guessed right in {guesses} guesses!";
+                ViewBag.addToHighScore = true;
                 //RedirectToAction("Index");
             }
             else
             {
                 ViewBag.response = $"You have guessed {guesses} so fare!";
                 ViewBag.response += $" The number i am thingking of is {((guess < number) ? "higher" : "lesser")} then you guessed";
+                ViewBag.addToHighScore = false;
             }
+            ViewBag.highScores =  Request.Cookies["HighScores"];
 
             return View();
         }
+        [HttpPost]
+        public IActionResult AddToHighScore(string name)
+        {
+            string highScoreLine=HttpContext.Session.GetInt32("Guesses").ToString();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                string highScores = Request.Cookies["HighScores"];
+                highScoreLine += "\t" + DateTime.Now.ToString()+"\t"+name;
+                if (highScores == null)
+                {
+                    Response.Cookies.Append("HighScores", highScoreLine);
+                }
+                else
+                {
+                    highScores += '\n' + highScoreLine;
+                    Response.Cookies.Append("HighScores", highScores);
+                }
+            }
 
+            return RedirectToAction(nameof(Index));
+          
+        }
     }
 }
